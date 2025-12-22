f7b6e68135c81aa06337eefcd546e04cb38758f3
mdestafadilah <desta.08b@gmail.com>
2025-12-22 09:46:17 +0700

    feat: initialize DataTables for user activity log with server-side processing and column filters

--- templates/js/profile.js
+++ templates/js/profile.js
@@ -1,27 +1,72 @@
 $(document).ready(function () {
-  // Fetch cities from API
   $("#logActivityTable").DataTable({
     processing: true,
     serverSide: true,
+    orderCellsTop: true,
     ajax: {
       url: baseUrl + "profile/ajax-log-activity",
-      type: "GET",
+      type: "POST",
+    },
+    initComplete: function (settings, json) {
+      var indexColumn = 0;
+
+      this.api()
+        .columns()
+        .every(function () {
+          var column = this;
+          var input = document.createElement("input");
+
+          $(input)
+            .attr("placeholder", "Search")
+            .addClass("form-control form-control-sm")
+            .appendTo($(".filterhead:eq(" + indexColumn + ")").empty())
+            .on("keyup", function () {
+              column.search($(this).val(), false, false, true).draw();
+            });
+
+          indexColumn++;
+        });
     },
     columns: [
-      { data: "no", orderable: false, searchable: false },
-      { data: "create_date" },
-      { data: "route" },
-      { data: "method", orderable: false },
-      { data: "browser" },
-      { data: "platform" },
-      { data: "is_ajax", orderable: false, searchable: false },
+      {
+        data: null,
+        orderable: false,
+        searchable: false,
+        render: function (data, type, row, meta) {
+          return meta.row + meta.settings._iDisplayStart + 1;
+        },
+      },
+      { data: "route", name: "user_apps_activity.route" },
+      { data: "method", name: "user_apps_activity.method" },
+      { data: "browser", name: "user_apps_activity.browser" },
+      { data: "platform", name: "user_apps_activity.platform" },
+      { data: "isAjax", name: "user_apps_activity.is_ajax" },
+      { data: "create_date", name: "user_apps_activity.create_date" },
     ],
-    order: [[1, "desc"]],
+    responsive: true,
     pageLength: 10,
+    lengthMenu: [
+      [10, 25, 50, -1],
+      [10, 25, 50, "All"],
+    ],
     language: {
-      processing: '<i class="fas fa-spinner fa-spin"></i> Loading...',
-      emptyTable: "No activity logs found",
+      search: "_INPUT_",
+      searchPlaceholder: "Search...",
+      lengthMenu: "Show _MENU_ entries",
+      info: "Showing _START_ to _END_ of _TOTAL_ entries",
+      infoEmpty: "Showing 0 to 0 of 0 entries",
+      infoFiltered: "(filtered from _MAX_ total entries)",
       zeroRecords: "No matching records found",
+      emptyTable: "No data available in table",
+      paginate: {
+        first: "First",
+        last: "Last",
+        next: "Next",
+        previous: "Previous",
+      },
+    },
+    drawCallback: function () {
+      $('[data-bs-toggle="tooltip"]').tooltip();
     },
   });
 });
